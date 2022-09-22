113904d1c04650038b39389979a4f60216a681c3
NakacwaOlivia <k.nakacwa.olivia@gmail.com>
2022-09-22 13:33:59 +0300

    initial commit

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,5 +1,7 @@
 <template>
+  <div class="background"></div>
   <div class="container">
+  <div class ="icon"></div>
     <h3>
       Final Year Projects Submission and <br />
       Review System
@@ -17,25 +19,37 @@
             <span class="fa-duotone fa-email" id="basic-addon1"
               ><font-awesome-icon icon="fa-duotone fa-email"
             /></span>
+            <!-- Email -->
             <input
+              v-model="email"
+              :rules="emailRules"
               type="email"
+              required
               class="form-control"
               id="exampleInputEmail1"
               aria-describedby="emailHelp"
               placeholder="Email"
             />
           </div>
+          <!-- Password -->
           <div class="mb-3">
             <span class="fa-duotone fa" id="basic-addon2"
               ><font-awesome-icon icon="fa-solid fa-user-secret"
             /></span>
             <input
-              type="password"
+              v-model="password"
+              :rules="passwordRules"
+              :type="passwordShow ? 'text' : 'password'"
+              placeholder="Password"
+              prepend-inner-icon="mdi-key"
+              :append-icon="passwordShow ? 'mdi-eye' : 'mdi-eye-off'"
+              @click:append="passwordShow = !passwordShow"
+              required
               class="form-control"
               id="exampleInputPassword1"
-              placeholder="Password"
             />
           </div>
+          <!-- Checkbox -->
           <div class="mb-3 form-check">
             <input
               type="checkbox"
@@ -46,18 +60,27 @@
               >Remember Me</label
             >
           </div>
-          <button type="submit" class="btn btn-primary">Login</button>
+          <!-- Submit Button -->
+          <button type="submit" class="btn btn-primary" @click="doLogin">Login</button>
         </form>
       </div>
       <br />
       <br />
       <h3>REGISTER AS A</h3>
-      <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+      <div
+        class="btn-toolbar mb-3"
+        role="toolbar"
+        aria-label="Toolbar with button groups"
+      >
         <div class="btn-group me-2" role="group" aria-label="First group">
-          <button type="button" class="btn btn-outline-secondary">STUDENT</button>
+          <button type="button" class="btn btn-outline-secondary">
+            STUDENT
+          </button>
         </div>
         <div class="btn-group me-3" role="group" aria-label="Second group">
-            <button type="button" class="btn btn-outline-secondary">SUPERVISOR</button>
+          <button type="button" class="btn btn-outline-secondary">
+            SUPERVISOR
+          </button>
         </div>
       </div>
       <div class="col-md-3"></div>
@@ -66,7 +89,56 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    passwordShow: false,
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+    ],
+    password: "",
+    passwordRules: [
+      (v) => !!v || "Password is required",
+      (v) => (v && v.length >= 6) || "Password must be 6  characters or more!",
+    ],
+  }),
+  methods:{
+    doLogin() {
+          if (this.email === "" || this.password === "") {
+             this.emptyFields = true;
+          } else {
+             alert("You are now logged in");
+          }
+       },
+  }
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+*{
+        align-items: center;
+}
+h2{
+    color: rgb(5, 172, 5);
+}
+.background {
+  background-image: url(background.jpg) !important;
+  height: 100%;
+  width: 100%;
+  display: block;
+  position: absolute;
+  top: 0;
+  background-size: cover;
+  background-color: green;
+}
+.icon{
+    background-image: url(mak.png);
+    height: 50%;
+    width: 80%;
+}
+.container{
+    background-color: white;
+
+}
+</style>
